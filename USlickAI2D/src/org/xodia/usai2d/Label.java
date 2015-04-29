41c3256618b86970335b48c977032f9a8bdd7dc0
Nemferno <jasperbae@gmail.com>
2015-04-29 18:43:01 -0400

    Label is its own instance similar to Button but with no listener.

--- USlickAI2D/src/org/xodia/usai2d/Label.java
+++ USlickAI2D/src/org/xodia/usai2d/Label.java
@@ -1,8 +1,12 @@
 package org.xodia.usai2d;
 
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 
-public class Label extends TextField {
+public class Label extends BasicUserInterface {
+	
+	private String text;
+	private TextOption option;
 	
 	public Label(GameContainer gc, float x, float y, float w, float h){
 		this(gc, "", x, y, w, h);
@@ -11,8 +15,41 @@ public class Label extends TextField {
 	public Label(GameContainer gc, String text, float x, float y, float w, float h){
 		super(gc, x, y, w, h);
 		
-		setEditable(false);
-		setText(text);
+		this.text = text;
+		this.option = TextOption.LEFT;
+	}
+	
+	public void render(Graphics g){
+		super.render(g);
+		
+		g.setColor(DEFAULT_TEXT);
+		switch(option){
+		case CENTER:
+			g.drawString(text, getX() + getWidth() / 2 - g.getFont().getWidth(text) / 2, getY() + getHeight() / 2 - g.getFont().getHeight(text) / 2);
+			break;
+		case LEFT:
+			g.drawString(text, getX(), getY() + getHeight() / 2 - g.getFont().getHeight(text) / 2);
+			break;
+		case RIGHT:
+			g.drawString(text, getX() + getWidth() - g.getFont().getWidth(text), getY() + getHeight() / 2 - g.getFont().getHeight(text) / 2);
+			break;
+		}
+	}
+	
+	public void setText(String text){
+		this.text = text;
+	}
+	
+	public void setTextOption(TextOption option){
+		this.option = option;
+	}
+	
+	public String getText(){
+		return text;
+	}
+	
+	public TextOption getTextOption(){
+		return option;
 	}
 	
 	public void setToolTip(String desc){
